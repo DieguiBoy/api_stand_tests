985a4b9ff5a3f612b6250ce4842d02c870348ed6
Diego <llanesdiego5@gmail.com>
2026-01-31 05:59:05 -0600

    Primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = ' https://cnt-5a2b893b-21a7-4716-8d74-1752b0386dbb.containerhub.tripleten-services.com'
+DOC_PATH = '/docs/'
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
